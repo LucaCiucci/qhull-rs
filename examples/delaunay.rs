74aebdf12ffcb5f09d79a741dd41e5d4e22ad5ad
LucaCiucci <luca.ciucci99@gmail.com>
2024-10-19 14:10:59 +0200

    Update delaunay example

--- examples/delaunay.rs
+++ examples/delaunay.rs
@@ -4,7 +4,7 @@ use std::error::Error;
 fn main() -> Result<(), Box<dyn Error>> {
     let qh = Qh::new_delaunay([[0.0, 0.0], [1.0, 0.0], [0.0, 1.0], [0.25, 0.25]])?;
 
-    for simplex in qh.simplices() {
+    for simplex in qh.simplices().filter(|f| !f.upper_delaunay()) {
         println!(
             "{:?}",
             simplex
@@ -18,7 +18,7 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let mut simplices = qh
         .simplices()
-        .filter(|f| !f.is_sentinel())
+        .filter(|f| !f.is_sentinel() && !f.upper_delaunay())
         .map(|f| {
             f.vertices()
                 .unwrap()
@@ -32,7 +32,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     simplices.sort();
     assert_eq!(
         simplices,
-        vec![vec![0, 1, 2], vec![0, 1, 3], vec![0, 2, 3], vec![1, 2, 3],]
+        vec![vec![0, 1, 3], vec![0, 2, 3], vec![1, 2, 3],]
     );
 
     Ok(())
